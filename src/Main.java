2b7eae4ba787a310b97a7d5bbd69dcfe02b2808b
vikahenkel <vhenkel39@gmail.com>
2023-08-23 17:24:49 +0200

    HW2

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Task 1
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        // Task 2
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        //Task 3
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        // Task 4
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        // Task 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10 / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        // Task 6
+        var boxerWeight1 = 78.2;
+        var boxerWeight2 = 82.7;
+        System.out.println(boxerWeight1 + boxerWeight2);
+        System.out.println(boxerWeight2 - boxerWeight1);
+
+        // Task7
+        var x = 78.2;
+        var y = 82.7;
+        var result = x - y;
+        System.out.println("x - y =" + result );
+        var weight =(boxerWeight2 % boxerWeight1);
+        System.out.println(boxerWeight2 % boxerWeight1);
+
+        // Task 8
+        // 1
+        var hour = 640;
+        var dayHour = 8;
+        var employees = 640 / 8;
+        System.out.println("a total of work " + employees + " in the company ");
+
+        // 2
+        var people = 80;
+        people = people + 94;
+        System.out.println(people);
+        var stundenTag = hour / people;
+        System.out.println("Wenn im Betrib " + people +  " arbeiten, dann kann jeder Mitarbeiter "  + stundenTag +  " stunden arbeiten ");
+
+
+
+
+
+
+
+
+    }
+}
